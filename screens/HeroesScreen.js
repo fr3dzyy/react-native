57e80c586ee399aad794b61220431341316914fa
Fredric Ryding <fredricr91@hotmail.com>
2022-06-14 18:04:11 +0200

    Added axios

--- screens/HeroesScreen.js
+++ screens/HeroesScreen.js
@@ -1,9 +1,46 @@
-import { Text, View } from 'react-native'
+import axios from 'axios'
+
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+import { useEffect, useState } from 'react'
 
 export default function Heroes() {
-  return (
+  const [heroes, setHeroes] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.opendota.com/api/heroStats').then((response) => {
+      setHeroes(response.data)
+    })
+  }, [])
+
+  const renderHero = ({ hero }) => (
     <View>
-      <Text>Heroes</Text>
+      <Text>{hero.localized_name}</Text>
     </View>
   )
+
+  return (
+    <>
+      <View>
+        <Text>Heroes!</Text>
+      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={heroes}
+          renderItem={renderHero}
+          keyExtractor={(hero) => hero.id}
+        />
+      </SafeAreaView>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+  },
+})
