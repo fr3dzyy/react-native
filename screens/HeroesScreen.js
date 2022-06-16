bd6958296f01524c922a41d144fc0e2c0ca26098
Fredric Ryding <fredricr91@hotmail.com>
2022-06-16 13:42:02 +0200

    Added switch - HeroesScreen.js

--- screens/HeroesScreen.js
+++ screens/HeroesScreen.js
@@ -1,11 +1,20 @@
 import axios from 'axios'
 
-import { FlatList, Image, SafeAreaView, StyleSheet, Text } from 'react-native'
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Switch,
+  Text,
+} from 'react-native'
 
 import { useEffect, useState } from 'react'
 
 export default function Heroes() {
   const [heroes, setHeroes] = useState([])
+  const [isEnabled, setIsEnabled] = useState(false)
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState)
 
   useEffect(() => {
     axios.get('https://api.opendota.com/api/heroStats').then((response) => {
@@ -27,21 +36,31 @@ export default function Heroes() {
           perform multiple roles. A heros appearance can be modified with
           equipment.
         </Text>
-        <FlatList
-          data={heroes}
-          style={styles.heroList}
-          renderItem={({ item }) => (
-            <>
-              <Text style={styles.heroText}>{item.localized_name}</Text>
-              <Image
-                alt="Hero Picture"
-                source={{ uri: 'https://steamcdn-a.akamaihd.net' + item.img }}
-                style={styles.img}
-              />
-            </>
-          )}
-          keyExtractor={(heroes) => heroes.id}
+        <Text style={styles.paragraphHeroes}>Show heroes!</Text>
+        <Switch
+          trackColor={{ false: '#767577', true: '#222222' }}
+          thumbColor={isEnabled ? '#222222' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
         />
+        {isEnabled && (
+          <FlatList
+            data={heroes}
+            style={styles.heroList}
+            renderItem={({ item }) => (
+              <>
+                <Text style={styles.heroText}>{item.localized_name}</Text>
+                <Image
+                  alt="Hero Picture"
+                  source={{ uri: 'https://steamcdn-a.akamaihd.net' + item.img }}
+                  style={styles.img}
+                />
+              </>
+            )}
+            keyExtractor={(heroes) => heroes.id}
+          />
+        )}
       </SafeAreaView>
     </>
   )
@@ -80,5 +99,11 @@ const styles = StyleSheet.create({
     color: '#ffc0cb',
     marginHorizontal: 25,
     marginVertical: 15,
+  },
+
+  paragraphHeroes: {
+    color: '#ffc0cb',
+    fontSize: 20,
+    marginTop: 10,
   },
 })
